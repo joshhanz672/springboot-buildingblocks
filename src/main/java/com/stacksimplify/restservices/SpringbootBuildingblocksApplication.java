4e8c7ce47538269433d9b0393baa97a09862af38
Josh Hanz <josh_hanz@hotmail.com>
2020-06-07 10:08:01 -0500

    Second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-//
+// 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
